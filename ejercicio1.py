a8368934ef32c39e5fc5d31900552467885d28ec
Sebastian Gaete <tian0330@gmail.com>
2016-03-17 14:25:43 -0300

    Ejercio 1

---
+++ ejercicio1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+def comparacion(x, y):
+	if(x > y):
+		return str(x)+" es mayor que "+str(y)
+	elif(x < y):
+		return str(x)+" es menor que "+str(y)
+	else:
+		return str(x)+" es igual que "+str(y)
+
+def 
+
+if __name__ == "__main__":
+	x = input("Ingrese un numero entero  ")
+	y = input("Ingrese un otro numero entero")
+	print (comparacion(x,y))
+
