09f1a4b17d99b0a5ba89026e4c0642716f77c264
Sebbastian Gaete <tian0330@gmail.com>
2016-04-28 11:26:45 -0300

    Trabajo respaldar shell

---
+++ respaldar.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#pone todos los argumentos en el arreglo direc
+direc=("$@")
+#cambiamos a la posicion de fuente
+cd "${direc[0]}" 
+#obtenemos el nombre de la carpeta donde estamos
+FUENTE="${PWD##*/}"
+#salimos al directorio que contiene la carpeta que vamos a copiar
+cd ..
+#asignamos la direccion de destino
+DESTINO=${direc[1]}
+#variables de la fecha
+D=$(date +%d)
+M=$(date +%m)
+Y=$(date +%Y)
+#creamos el nombre del archivo con la fecha y el nombre de la carpeta a respaldar
+FILENAME="$Y-$M-$D-$FUENTE"
+#creamos el zip y lo enviamos a la direccion de destino
+zip -r $DESTINO/$FILENAME.zip $FUENTE
